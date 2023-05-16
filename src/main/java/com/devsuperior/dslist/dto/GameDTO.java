d074540fdfb5417ea1b84544685d45f279d6b817
lucas1806lu <lchaves.20@gmail.com>
2023-05-16 11:08:59 -0300

    “GET_lists”

--- src/main/java/com/devsuperior/dslist/dto/GameDTO.java
+++ src/main/java/com/devsuperior/dslist/dto/GameDTO.java
@@ -16,6 +16,7 @@ public class GameDTO {
 	private String shortDescription;
 	private String longDescription;
 	
+	//copia entidades para objetos
 	public GameDTO(Game entity) {
 		BeanUtils.copyProperties(entity, this);
 	}
